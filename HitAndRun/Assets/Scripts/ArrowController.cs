b3c94d15fea6d8b18b7380a62ee8237cedc2220b
Dizhen Lu <dizhenlu@usc.edu>
2015-02-23 18:51:35 -0800

    Arrow and Background Sprites Added, ArrowController Created, and Bounds Enforced on Arrow

---
+++ HitAndRun/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+	public float moveSpeed;
+	private Vector3 moveDirection;
+	public float turnSpeed;
+
+	[SerializeField]
+	private PolygonCollider2D[] colliders;
+	private int currentColliderIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		moveDirection = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 currentPosition = transform.position;
+		if( Input.GetButton("Fire1") ) {
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+		}
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+
+		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = 
+			Quaternion.Slerp( transform.rotation, 
+			                 Quaternion.Euler( 0, 0, targetAngle ), 
+			                 turnSpeed * Time.deltaTime );
+		EnforceBounds();
+	}
+
+	public void SetColliderForSprite( int spriteNum )
+	{
+		colliders[currentColliderIndex].enabled = false;
+		currentColliderIndex = spriteNum;
+		colliders[currentColliderIndex].enabled = true;
+	}
+
+	private void EnforceBounds()
+	{
+		Vector3 newPosition = transform.position; 
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+		
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+		float yMax = mainCamera.orthographicSize;
+
+		if ( newPosition.x < xMin || newPosition.x > xMax ) {
+			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			moveDirection.x = -moveDirection.x;
+		}
+
+		if (newPosition.y < -yMax || newPosition.y > yMax) {
+			newPosition.y = Mathf.Clamp( newPosition.y, -yMax, yMax );
+			moveDirection.y = -moveDirection.y;
+		}
+		transform.position = newPosition;
+	}
+}
