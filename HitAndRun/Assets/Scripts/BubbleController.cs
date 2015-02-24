d8d8e59693e2ef8c7298b8e3b84404b49fa60752
Rex Zeng <zcnrex@gmail.com>
2015-02-23 23:41:40 -0800

    bubbles spawn randomly and move in random directions.

---
+++ HitAndRun/Assets/Scripts/BubbleController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System;
+
+public class BubbleController : MonoBehaviour {
+	private float moveSpeed = 0.1f; 
+	private float turnSpeed = 0.3f;
+	private float xMax;
+	private float yMax;
+	private Vector3 moveDirection;
+	private Camera myCamera;
+	private Vector3 moveToward;
+	private Vector3 currentPosition;
+	private Random random;
+	// Use this for initialization
+	void Start () {
+		myCamera = Camera.main;
+		moveToward = new Vector3(Random.Range(-xMax, xMax),
+		                         Random.Range(-yMax, yMax), 
+		                         transform.position.z );
+		xMax = myCamera.aspect * myCamera.orthographicSize;
+		yMax = myCamera.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentPosition = transform.position;
+		moveDirection = moveToward - currentPosition;
+		if (Mathf.Abs(moveDirection.x) <= 0.3 || Mathf.Abs(moveDirection.y) <= 0.3) {
+			moveToward = new Vector3(Random.Range(-xMax, xMax),
+			                         Random.Range(-yMax, yMax), 
+			                         transform.position.z );
+			moveDirection = moveToward - currentPosition;
+		}
+		moveDirection.z = 0;
+//		moveDirection.Normalize();
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+		
+		float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = 
+			Quaternion.Slerp (transform.rotation, 
+			                  Quaternion.Euler (0, 0, targetAngle), 
+			                  turnSpeed * Time.deltaTime);
+	}
+}
---
+++ HitAndRun/Assets/Scripts/BubbleCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BubbleCreator: MonoBehaviour {
+	public float minSpawnTime = 2f; 
+	public float maxSpawnTime = 5f; 
+	
+	public GameObject bubblePrefab;
+	
+	void Start () 
+	{
+		Invoke("SpawnBubble",minSpawnTime);
+	}
+	
+	void SpawnBubble() {
+		Camera camera = Camera.main;
+//		Vector3 cameraPos = camera.transform.position;
+		float xMax = camera.aspect * camera.orthographicSize;
+		float xRange = camera.aspect * camera.orthographicSize * 1.75f;
+		float yMax = camera.orthographicSize - 0.5f;
+		
+		Vector3 bubblePos = 
+			new Vector3(Random.Range(xMax - xRange, xMax),
+			            Random.Range(-yMax, yMax),
+			            bubblePrefab.transform.position.z);
+		
+		Instantiate(bubblePrefab, bubblePos, Quaternion.identity);
+		
+		Invoke("SpawnBubble", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+}
